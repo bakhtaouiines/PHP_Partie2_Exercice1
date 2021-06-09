e91d8c221b224707ad321f1bdd58abdb2af751f6
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 15:13:22 +0200

    correction ajout HTML

--- exercice2.1.php
+++ exercice2.1.php
@@ -1,11 +1,25 @@
-<?php
-$age = 4;
-if ($age >= 18)
-{
-    echo "Vous êtes majeur!";
-}
-else
-{
-    echo "Vous êtes mineur!";
-}
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2.1</title>
+</head>
+<body>
+    <h1>Exercice 1 Partie 2 : Les conditions PHP</h1>
+    <p>
+        <?php
+            $age = 4;
+            if ($age >= 18)
+            {
+                echo "Vous êtes majeur!";
+            }
+            else
+            {
+                echo "Vous êtes mineur!";
+            }
+        ?>
+    </p>
+</body>
+</html>
